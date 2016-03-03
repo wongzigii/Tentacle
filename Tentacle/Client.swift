a123bf7c466b2bcef9cfce3d9495e245f7bc1db2
Matt Diephouse <matt@diephouse.com>
2016-03-03 14:26:44 -0500

    Add a Client class

---
+++ Tentacle/Client.swift
@@ -0,0 +1,14 @@
+//
+//  Client.swift
+//  Tentacle
+//
+//  Created by Matt Diephouse on 3/3/16.
+//  Copyright © 2016 Matt Diephouse. All rights reserved.
+//
+
+import Foundation
+
+/// A GitHub API Client
+public final class Client {
+	
+}
