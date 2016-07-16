4f595f0c4c5f9e25ec17f99b3427f19aa40dc95b
Romain Pouclet <romain.pouclet@gmail.com>
2016-07-15 20:01:30 -0400

    Fix formatting

--- Tests/IssuesTests.swift
+++ Tests/IssuesTests.swift
@@ -13,14 +13,34 @@ import XCTest
 class IssuesTests: XCTestCase {
     
     func testDecodedPalleasOpensourceIssues() {
-        let palleasOpensource = User(ID: "15802020",
-                                     login: "Palleas-opensource",
-                                     URL: NSURL(string: "https://api.github.com/users/Palleas-opensource")!,
-                                     avatarURL: NSURL(string: "https://avatars.githubusercontent.com/u/15802020?v=3")!)
+        let palleasOpensource = User(
+            ID: "15802020",
+            login: "Palleas-opensource",
+            URL: NSURL(string: "https://api.github.com/users/Palleas-opensource")!,
+            avatarURL: NSURL(string: "https://avatars.githubusercontent.com/u/15802020?v=3")!
+        )
 
-        let shipItMilestone = Milestone(ID: "1881390", number: 1, state: .Open, title: "Release this app", contentDescription: "That'd be cool", creator: palleasOpensource, numberOfOpenIssues: 1, numberOfClosedIssues: 0, createdAt: NSDateFormatter.ISO8601.dateFromString("2016-07-13T16:56:48Z")!, updatedAt: NSDateFormatter.ISO8601.dateFromString("2016-07-13T16:56:57Z")!, closedAt: nil, dueOn: NSDateFormatter.ISO8601.dateFromString("2016-07-25T04:00:00Z")!, URL: NSURL(string: "https://api.github.com/repos/Palleas-opensource/Sample-repository/milestones/1")!)
+        let shipItMilestone = Milestone(
+            ID: "1881390",
+            number: 1,
+            state: .Open,
+            title: "Release this app",
+            contentDescription: "That'd be cool",
+            creator: palleasOpensource,
+            numberOfOpenIssues: 1,
+            numberOfClosedIssues: 0,
+            createdAt: NSDateFormatter.ISO8601.dateFromString("2016-07-13T16:56:48Z")!,
+            updatedAt: NSDateFormatter.ISO8601.dateFromString("2016-07-13T16:56:57Z")!,
+            closedAt: nil,
+            dueOn: NSDateFormatter.ISO8601.dateFromString("2016-07-25T04:00:00Z")!,
+            URL: NSURL(string: "https://api.github.com/repos/Palleas-opensource/Sample-repository/milestones/1")!
+        )
 
-        let updateReadmePullRequest = PullRequest(URL: NSURL(string: "https://github.com/Palleas-opensource/Sample-repository/pull/3")!, diffURL: NSURL(string: "https://github.com/Palleas-opensource/Sample-repository/pull/3.diff")!, patchURL: NSURL(string: "https://github.com/Palleas-opensource/Sample-repository/pull/3.patch")!)
+        let updateReadmePullRequest = PullRequest(
+            URL: NSURL(string: "https://github.com/Palleas-opensource/Sample-repository/pull/3")!,
+            diffURL: NSURL(string: "https://github.com/Palleas-opensource/Sample-repository/pull/3.diff")!,
+            patchURL: NSURL(string: "https://github.com/Palleas-opensource/Sample-repository/pull/3.patch")!
+        )
 
         let expected = [
             Issue(id: 165458041,
@@ -46,9 +66,20 @@ class IssuesTests: XCTestCase {
                 title: "This issue is open",
                 body: "Issues are pretty cool.",
                 user: palleasOpensource,
-                labels: [Label(name: "bug", color: SWColor(hexString: "ee0701")!),
-                    Label(name: "duplicate", color: SWColor(hexString: "cccccc")!),
-                    Label(name: "enhancement", color: SWColor(hexString: "84b6eb")!)],
+                labels: [
+                    Label(
+                        name: "bug",
+                        color: SWColor(hexString: "ee0701")!
+                    ),
+                    Label(
+                        name: "duplicate",
+                        color: SWColor(hexString: "cccccc")!
+                    ),
+                    Label(
+                        name: "enhancement",
+                        color: SWColor(hexString: "84b6eb")!
+                    )
+                ],
                 assignee: palleasOpensource,
                 milestone: shipItMilestone,
                 locked: false,
