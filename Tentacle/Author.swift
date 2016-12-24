3101d286ad8293d6395d740437ad1cd4ca63c845
Romain Pouclet <romain.pouclet@gmail.com>
2016-12-24 09:52:50 +0100

    Add public init for Author

--- Tentacle/Author.swift
+++ Tentacle/Author.swift
@@ -13,6 +13,11 @@ import Argo
 public struct Author {
     let name: String
     let email: String
+
+    public init(name: String, email: String) {
+        self.name = name
+        self.email = email
+    }
 }
 
 extension Author: Encodable {
